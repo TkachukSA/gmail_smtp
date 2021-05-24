b768c8c01691ecf1686e7ffc4b76a72ff9e30935
asa121asa <crafta.net@gmail.com>
2021-05-25 01:55:33 +0300

    added env

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+/*
 const express = require('express');
 const nodemailer = require('nodemailer');
 const app = express();
@@ -5,6 +6,7 @@ const app = express();
 
 const bodyParser = require('body-parser')
 const cors = require('cors');
+
 
 app.use(cors());
 app.use(bodyParser.urlencoded({extendent: true}))
@@ -63,4 +65,66 @@ let port = process.env.PORT || 3010
 
 app.listen(port, () => {
     console.log('port 3010')
+})
+*/
+const express = require('express')
+const nodemailer = require("nodemailer");
+const cors = require('cors')
+const bodyParser = require('body-parser')
+
+const app = express()
+const port = process.env.PORT || 3010
+
+app.use(cors({
+    origin: ['http://localhost:3000']
+}))
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: false }))
+
+// parse application/json
+app.use(bodyParser.json())
+
+
+
+let smtp_login = process.env.SMTP_LOGIN || "---";
+let smtp_password = process.env.SMTP_PASSWORD || "---";
+
+app.get('/', (req, res) => {
+    res.send('Hello World!')
+})
+
+app.post('/sendMessage', async (req, res) => {
+
+    let transporter = nodemailer.createTransport({
+        service: "gmail",
+        auth: {
+            user: smtp_login, // generated ethereal user
+            pass: smtp_password, // generated ethereal password
+        },
+    });
+
+    let {name, email, message } = req.body;
+    // send mail with defined transport object
+
+    let info = await transporter.sendMail({
+        from: 'HR', // sender address
+        to: "crafta.net@gmail.com", // list of receivers
+        subject: 'HR WANTS ME', // Subject Line
+
+        html: `<b>Message to my portfolio page</b>
+    <div>
+        name: ${name}
+    </div>
+    <div>
+        email: ${email}
+    </div>
+    <div>
+       ${message}
+    </div>`
+    });
+    res.send('Ok')
+})
+
+app.listen(port, () => {
+    console.log(`Example app listening at http://localhost:${port}`)
 })
