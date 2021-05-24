80af4ead3ea7f9635f40a3e646ab7bf3d5afa406
asa121asa <crafta.net@gmail.com>
2021-05-24 21:24:25 +0300

    first comit

---
+++ index.js
@@ -0,0 +1,55 @@
+const express = require('express');
+const nodemailer = require('nodemailer');
+const app = express();
+
+
+const bodyParser = require('body-parser')
+const cors = require('cors');
+
+app.use(cors());
+app.use(bodyParser.urlencoded({extendent: true}))
+app.use(express.json())
+
+let transporter = nodemailer.createTransport({
+    service: "gmail",
+    auth: {
+        user: "asa121asaasa@gmail.com",
+        pass: "asa121asa"
+    }
+})
+
+app.post('/sendMessge', async (req, res) => {
+
+    let {
+        email,
+        message,
+        name
+    } = req.body
+    let info = await transporter.sendMail({
+        form: "my profile page",
+        to: "crafta.net@gmail.com",
+        subject: "hr wonts me",
+        html: `<b>message from Portfolio page</b>
+       <div>
+          name: ${name}
+       </div>
+       <div>
+          from: ${email}
+        </div>
+        <div>
+          message: ${message}
+        </div>
+`
+    })
+    res.send('ok')
+    res.send(console.log(req.body))
+
+})
+
+app.get('/', (req, res) => {
+    res.send('HellWorld')
+});
+
+app.listen(3010, () => {
+    console.log('port 3010')
+})
