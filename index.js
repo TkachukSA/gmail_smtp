786802b3ba395d496bc2d2ae5c77cda43be01b3e
asa121asa <crafta.net@gmail.com>
2021-05-25 01:03:04 +0300

    added env

--- index.js
+++ index.js
@@ -49,12 +49,18 @@ app.post('/sendMessge', async (req, res) => {
     res.send(console.log(req.body))
 
 })
-
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", '*');
+    res.header("Access-Control-Allow-Credentials", true);
+    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
+    res.header("Access-Control-Allow-Headers", 'Origin,X-Requested-With,Content-Type,Accept,content-type,application/json');
+    next();
+});
 app.get('/', (req, res) => {
     res.send('Hello World')
 });
 let port = process.env.PORT || 3010
 
-app.listen(port, () => {
+app.listen(3010, () => {
     console.log('port 3010')
 })
