ad6e94a8293fc4ab158e1c8a6d1b850177aecedd
asa121asa <crafta.net@gmail.com>
2021-05-25 14:27:08 +0300

    added env

--- index.js
+++ index.js
@@ -1,4 +1,3 @@
-/*
 const express = require('express');
 const nodemailer = require('nodemailer');
 const app = express();
@@ -19,8 +18,8 @@ let smtp_password = process.env.smtp_password || '--'
 let transporter = nodemailer.createTransport({
     service: "gmail",
     auth: {
-        user: smtp_login,
-        pass: smtp_password
+        user: smtp_login,  //'asa121asaasa@gmail.com',
+        pass: smtp_password // 'asa121asa'
     }
 })
 
@@ -65,66 +64,4 @@ let port = process.env.PORT || 3010
 
 app.listen(port, () => {
     console.log('port 3010')
-})
-*/
-const express = require('express')
-const nodemailer = require("nodemailer");
-const cors = require('cors')
-const bodyParser = require('body-parser')
-
-const app = express()
-const port = process.env.PORT || 3010
-
-app.use(cors({
-    origin: ['http://localhost:3000']
-}))
-// parse application/x-www-form-urlencoded
-app.use(bodyParser.urlencoded({ extended: false }))
-
-// parse application/json
-app.use(bodyParser.json())
-
-
-
-let smtp_login = process.env.SMTP_LOGIN || "---";
-let smtp_password = process.env.SMTP_PASSWORD || "---";
-
-app.get('/', (req, res) => {
-    res.send('Hello World!')
-})
-
-app.post('/sendMessage', async (req, res) => {
-
-    let transporter = nodemailer.createTransport({
-        service: "gmail",
-        auth: {
-            user: smtp_login, // generated ethereal user
-            pass: smtp_password, // generated ethereal password
-        },
-    });
-
-    let {name, email, message } = req.body;
-    // send mail with defined transport object
-
-    let info = await transporter.sendMail({
-        from: 'HR', // sender address
-        to: "crafta.net@gmail.com", // list of receivers
-        subject: 'HR WANTS ME', // Subject Line
-
-        html: `<b>Message to my portfolio page</b>
-    <div>
-        name: ${name}
-    </div>
-    <div>
-        email: ${email}
-    </div>
-    <div>
-       ${message}
-    </div>`
-    });
-    res.send('Ok')
-})
-
-app.listen(port, () => {
-    console.log(`Example app listening at http://localhost:${port}`)
 })
